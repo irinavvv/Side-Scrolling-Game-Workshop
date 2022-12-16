c161682dd7dc3f8015fac32f6b13355ff5dd2034
irinavvv <105821669+irinavvv@users.noreply.github.com>
2022-12-16 23:28:52 +0200

    move wizard up

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -8,13 +8,19 @@ function start (state, game) {
 function gameLoop(state, game) {
     const {wizard} = state;
     const {wizardEl} = game;
+
 // move  wizard
 if (state.keys.KeyD) {
-    wizard.posX += 2;
+    wizard.posX += 10;
 }
 
+if (state.keys.KeyW) {
+    wizard.posY -= 10;
+}
 //render game
 wizardEl.style.left = state.wizard.posX + "px"
+wizardEl.style.top = state.wizard.posY + "px"
+
 
 window.requestAnimationFrame(gameLoop.bind(null, state, game));
 }
